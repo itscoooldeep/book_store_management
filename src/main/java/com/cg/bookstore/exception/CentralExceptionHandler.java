487e3b77733bb49c457acb38e1bf048f1ae1e44b
sanju212177 <gusainsanjay21@gmail.com>
2022-07-13 17:15:51 +0530

    book management project started

---
+++ src/main/java/com/cg/bookstore/exception/CentralExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.cg.bookstore.exception;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+@RestControllerAdvice
+public class CentralExceptionHandler {
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<String> exceptionaHandler1(InvalidFormatException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> exceptionaHandler1(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.joining(","));
+        return new ResponseEntity<String>(message, HttpStatus.NOT_ACCEPTABLE);
+    }
+}
--- src/main/java/com/cg/bookstore/service/IReviewService.java
+++ src/main/java/com/cg/bookstore/service/IReviewService.java
@@ -9,16 +9,21 @@ import com.cg.bookstore.entities.Review;
 public interface IReviewService {
 
 	public List<Review> listAllReviews();
+
 	public Review addReview(Review review);
+
 	public Review deleteReview(Review review);
+
 	public Review updateReview(Review review);
+
 	public Review viewReview(Review review);
+
 	public List<Review> listAllReviewsByBook(Book book);
+
 	public List<Review> listAllReviewsByCustomer(Customer c);
+
 	public List<Book> listMostFavoredBooks();
-<<<<<<< HEAD
-	public List<Review> listAllReviewByCustomer(customer customer);
-=======
+
 	public List<Review> listAllReviewByCustomer(Customer customer);
->>>>>>> 9317396 ( book management project started)
+
 }
