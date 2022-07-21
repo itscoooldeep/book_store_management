4e4d121667913841541e507fb39ddcb9c0a678ec
Varun Kiroriwal <88306506+VarunKiroriwal@users.noreply.github.com>
2022-07-21 10:29:40 +0530

    Add files via upload

--- src/main/java/com/cg/bookstore/entities/Customer.java
+++ src/main/java/com/cg/bookstore/entities/Customer.java
@@ -1,14 +1,69 @@
-package com.cg.bookstore.entities;
+package com.cg.bookstore.entity;
 
-import java.time.LocalDate;
 
-public class Customer {
+import javax.persistence.*;
+import javax.validation.Valid;
 
-	private int customerId;
-	private String email;
-	private String fullName;
-	private String password;
-	private Address address;
-	private String mobileNumber;
-	private LocalDate registerOn;
+
+@Entity
+public class Customer extends User {
+
+
+    private String fullName;
+
+
+    @Valid
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "addressId")
+    private Address address;
+    private String mobileNumber;
+
+
+    public Customer() {
+        super();
+    }
+
+
+    public Customer(String fullName, Address address, String mobileNumber) {
+        super();
+        this.fullName = fullName;
+        this.address = address;
+        this.mobileNumber = mobileNumber;
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "fullName='" + fullName + '\'' +
+                ", address=" + address +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                '}';
+    }
 }
+
