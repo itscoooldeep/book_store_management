5dbb32254de08717d2ed2b4a127072c34e9d693b
Varun Kiroriwal <88306506+VarunKiroriwal@users.noreply.github.com>
2022-07-21 10:35:58 +0530

    Add files via upload

--- src/main/java/com/cg/bookstore/entities/Address.java
+++ src/main/java/com/cg/bookstore/entities/Address.java
@@ -1,11 +1,81 @@
-package com.cg.bookstore.entities;
+package com.cg.bookstore.entity;
 
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Customer_address")
 public class Address {
 
-	private int addressId;
-	private String address;
-	private String city;
-	private String country;
-	private String pincode;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int addressId;
+    private String houseAddress;
+    private String state;
+    private String country;
+    private String pinCode;
+
+    public Address() {
+    }
+
+    public Address(int addressId, String houseAddress, String state, String country, String pinCode) {
+        this.addressId = addressId;
+        this.houseAddress = houseAddress;
+        this.state = state;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getHouseAddress() {
+        return houseAddress;
+    }
+
+    public void setHouseAddress(String houseAddress) {
+        this.houseAddress = houseAddress;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId=" + addressId +
+                ", houseAddress='" + houseAddress + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", pinCode='" + pinCode + '\'' +
+                '}';
+    }
 
 }
